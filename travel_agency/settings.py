e2b264ce84de538a6b6eb98338bbe79f8f2987c9
NawalBDA <test4tech1@gmail.com>
2026-01-17 21:40:50 +0100

    settings db

--- travel_agency/settings.py
+++ travel_agency/settings.py
@@ -65,14 +65,19 @@ CSRF_TRUSTED_ORIGINS = [
     "https://oma-travels.onrender.com"
 ]
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600,
-        ssl_require=bool(os.environ.get("DATABASE_URL"))
-    )
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
