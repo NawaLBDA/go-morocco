1bf40e86f06f31cf642c00d23266721bc2b15d21
NawalBDA <test4tech1@gmail.com>
2026-01-18 11:18:16 +0100

    cloudinary config

--- travel_agency/settings.py
+++ travel_agency/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-dev-key-change-later'
 DEBUG = True
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -20,9 +20,17 @@ INSTALLED_APPS = [
     'apps.core.apps.CoreConfig',
     'apps.reservations',
     'widget_tweaks',
+    'cloudinary',
+    'cloudinary_storage',
 
 ]
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    'API_KEY': os.environ.get("CLOUDINARY_API_KEY"),
+    'API_SECRET': os.environ.get("CLOUDINARY_API_SECRET"),
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -51,33 +59,24 @@ TEMPLATES = [
         },
     },
 ]
-STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
-STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
-import dj_database_url
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STRIPE_SECRET_KEY = 'sk_test_51SpxfZ4YMQsgoNZ714OWYuDTtH4gfUQOrBt9fQ4NaolGxiwydO6OxdGM2qzxwM0W4C1xCyMTPTcYr7nMleU7dfoc00fggPuC2j'
+STRIPE_PUBLIC_KEY = 'pk_test_51SpxfZ4YMQsgoNZ7pIlbVjDkCpGI49QCtLazSvXcx4fOZi3NcjR2MwdJGNjC7b3tlwJiz9UzfMendoL5toA2OYAv00t7GomWAM'
 
-MIDDLEWARE.insert(
-    1, "whitenoise.middleware.WhiteNoiseMiddleware"
-)
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-CSRF_TRUSTED_ORIGINS = [
-    "https://oma-travels.onrender.com"
-]
-
-DATABASE_URL = os.environ.get("DATABASE_URL")
-
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'travel_agency',   # ✅ base déjà créée
+        'USER': 'root',
+        'PASSWORD': 'P@ss123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
@@ -103,3 +102,4 @@ MESSAGE_TAGS = {
     messages.WARNING: 'warning',
     messages.INFO: 'info',
 }
+
