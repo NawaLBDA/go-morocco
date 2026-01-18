3a119f3d783742fe09a0e5b91d9d1e7933c0d0d4
NawalBDA <test4tech1@gmail.com>
2026-01-18 12:59:08 +0100

    Fix static files for production

--- travel_agency/settings.py
+++ travel_agency/settings.py
@@ -153,7 +153,16 @@ CLOUDINARY_STORAGE = {
     "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
     "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
 }
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
+cloudinary.config(
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.environ.get("CLOUDINARY_API_KEY"),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
+    secure=True
+)
 # ==============================
 # STRIPE
 # ==============================
