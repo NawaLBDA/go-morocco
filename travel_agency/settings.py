b31fa0e065df347d0c3e75a5c33b5c7e048e1a78
NawalBDA <test4tech1@gmail.com>
2026-03-05 00:41:00 +0000

    fix templates config APP_DIRS for cached loader

--- travel_agency/settings.py
+++ travel_agency/settings.py
@@ -88,7 +88,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
+        # APP_DIRS must be False when we provide custom loaders below
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
