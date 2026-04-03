5819a6a6e0f197729fe6d427e4e3b8c02a5a3bc8
NawalBDA <test4tech1@gmail.com>
2026-04-03 16:47:01 +0100

    Ireland assets: use about1-ir + logo-ir1

--- apps/core/context_processors.py
+++ apps/core/context_processors.py
@@ -75,7 +75,7 @@ def sections_processor(request):
     try:
         base_url = request.build_absolute_uri('/')
         if country == 'ireland':
-            logo = static_url('img/logo-ir.png')
+            logo = static_url('img/logo-ir1.png')
             area_served = 'IE'
         else:
             logo = static_url('img/logo4.webp')
